59e8bb6030ad07670d47201c8ac4d477ddde7669
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2020-04-12 18:07:44 +0200

    Fix vignette

--- vignettes/scatter_matrix_simplex.R
+++ vignettes/scatter_matrix_simplex.R
@@ -1,17 +1,23 @@
 # Scatter matrix plot for data on a simplex
 #
 
-library(ggplot2)
+requireNamespace('ggplot2')
 requireNamespace('GGally')
 
 scatter_plot_01 <- function(data, mapping, ...) {
-   ggplot(data = data, mapping = mapping) +
-      geom_point(...) +
-      scale_x_continuous(limits = c(0,1), expand = c(0,0)) +
-      scale_y_continuous(limits = c(0,1), expand = c(0,0))
+   ggplot2::ggplot(data = data, mapping = mapping) +
+      ggplot2::geom_point(...) +
+      ggplot2::scale_x_continuous(limits = c(0,1), expand = c(0,0)) +
+      ggplot2::scale_y_continuous(limits = c(0,1), expand = c(0,0))
 }
 
 scatter_matrix_simplex <- function(df) {
-   GGally::ggpairs(df, upper = list(continuous = scatter_plot_01), diag = 'blank', lower = 'blank', labeller = label_parsed) +
-   theme_bw()
+   GGally::ggpairs(
+      df,
+      upper = list(continuous = scatter_plot_01),
+      diag = 'blank',
+      lower = 'blank',
+      labeller = ggplot2::label_parsed
+   ) +
+   ggplot2::theme_bw()
 }
